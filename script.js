01297f8a8e93b744d1d0aad84f02e44675528a62
viktor7utr-glitch <viktor7utr@gmail.com>
2026-03-02 12:19:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const card = document.getElementById("card");
+const home = document.getElementById("home");
 
+const messages = {
+1:"Поздравление №1",
+2:"Поздравление №2",
+3:"Поздравление №3",
+4:"Поздравление №4"
+};
+
+function openCard(id){
+
+document.getElementById("mainMusic").pause();
+
+home.classList.remove("active");
+card.classList.add("active");
+
+card.innerHTML = `
+<img src="images/guy${id}.jpg" class="bigPhoto">
+<div class="message">${messages[id]}</div>
+<button onclick="goHome()">Назад</button>
+<audio autoplay loop>
+<source src="music/guy${id}.mp3">
+</audio>
+`;
+}
+
+function goHome(){
+card.classList.remove("active");
+home.classList.add("active");
+document.getElementById("mainMusic").play();
+}
